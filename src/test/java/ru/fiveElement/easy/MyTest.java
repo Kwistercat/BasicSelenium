51047467b65366a51f8b3337f20ed747ff9cf240
Kwister <tagirza@yandex.ru>
2023-06-28 19:10:14 +0300

    errorTextSearchNotFoundTest added

--- src/test/java/ru/fiveElement/easy/MyTest.java
+++ src/test/java/ru/fiveElement/easy/MyTest.java
@@ -2,6 +2,7 @@ package ru.fiveElement.easy;
 
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -17,16 +18,17 @@ public class MyTest {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\IdeaProjects\\BasicSelenium\\BasicSelenium\\src\\main\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
-
     }
 
     @Test
-    public void firstTest() {
+    public void errorTextSearchNotFoundTest() {
         driver.get("https://5element.by/");
         WebElement searchField = driver.findElement(By.xpath("//*[@id=\"app\"]/header//form[2]/input[2]"));
-        searchField.sendKeys("123", Keys.ENTER);
-
+        searchField.sendKeys("ТЕЛЕВИЗОР АНДРОИД", Keys.ENTER);
+        WebElement errorText = driver.findElement(By.xpath("//div[text() = 'К сожалению, страница не найдена (ошибка 404)']"));
+        Assert.assertEquals("К сожалению, страница не найдена (ошибка 404)", errorText.getText());
     }
+
 
     @After
     public void tearDown() {
